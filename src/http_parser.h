3e5d3335d8d49c1fb569f0389d8b7719c1a60b96
sora <123005041+kazurem@users.noreply.github.com>
2025-03-17 17:08:39 +0500

    adding test to gitignore

--- src/http_parser.h
+++ src/http_parser.h
@@ -1,7 +1,25 @@
 #pragma once
 
+#include <iostream>
+
 class HTTPMessage
 {
+    std::string message;
+
+    std::string resource_path;
+    std::string method;
+    std::string http_version;
+
+    std::string content_type;
+    std::string content_length;
+
+
+    //Get message from user-agent request buffer
+    //parse it
+    //using if conditions and status codes, perform the appropriate actions
+    //build the response
+    //send back the response to the HTTP server
+    //from there the http server sends the response to the user agent
 
 };
 
