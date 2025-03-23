a5eee5d848bcbdb2b2e42c83a1ac3a925c093b04
kazurem <astraignis1@gmail.com>
2025-03-23 03:01:21 -0700

    function renaming in http_server.cpp and some code restructuring

--- src/http_server.h
+++ src/http_server.h
@@ -26,11 +26,14 @@ namespace http
         int new_socket_file_descriptor;
         struct sockaddr_in socket_address;
         unsigned int socket_address_length;
+        
         static const int BUFFER_SIZE = 30760;
         std::string server_message;
+
         in_addr acceptConnection(int &new_socket_fd);
         void sendResponse(std::string response, const in_addr client_addr, std::string status_line);
-        void startServer();
+        void createAndBindSocket();
+
         HTTPRequest req_handler;
         HTTPResponse response;
         Logger *logger;
